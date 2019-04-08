a8f5a9485cfad276c25b0e811a6ffd80adfb4631
AnteVuletic <antevuletic1995@gmail.com>
2019-04-08 09:26:20 +0200

    Project init

---
+++ tic_tac_toe/src/Components/Entry/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Game from '../Game/game';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Game></Game>
+      </div>
+    );
+  }
+}
+
+export default App;
